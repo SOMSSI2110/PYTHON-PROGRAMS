8978eccab65cc77e54d81ef41f0bb3f6c703a637
SOMSSI2110 <70804494+SOMSSI2110@users.noreply.github.com>
2020-09-07 19:29:41 +0530

    Add files via upload

---
+++ area-filename.py
@@ -0,0 +1,18 @@
+print("WELCOME");
+val=float(input("Enter Radius"));
+area=3.14*val*val;
+print("Area of the Circle", area);
+print("THANKYOU");
+
+
+print("WELCOME");
+n=str(input("enter the filename"))
+s=[]
+s=n.split(".")
+if s[-1]==("py"):
+    print("python")
+elif s[-1]==("ai"):
+    print("adobe illustrator")
+elif s[-1]==("jpeg"):
+    print("jpg image")
+print("THANKYOU");
