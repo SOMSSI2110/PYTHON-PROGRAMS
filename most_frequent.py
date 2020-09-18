237d563cf76c6e37a979cbf35ba9805e5d8fc4c3
SOMSSI2110 <70804494+SOMSSI2110@users.noreply.github.com>
2020-09-18 22:46:29 +0530

    Add files via upload

---
+++ most_frequent.py
@@ -0,0 +1,19 @@
+def most_frequent(m):
+    a=m.count("A");     b=m.count("B");     c=m.count("C");     d=m.count("D");
+    e=m.count("E");     f=m.count("F");     g=m.count("G");     h=m.count("H");
+    i=m.count("I");     j=m.count("J");     k=m.count("K");     l=m.count("L");
+    M=m.count("M");     n=m.count("N");     o=m.count("O");     p=m.count("P");
+    q=m.count("Q");     r=m.count("R");     s=m.count("S");     t=m.count("T");
+    u=m.count("U");     v=m.count("V");     w=m.count("W");     x=m.count("X");
+    y=m.count("Y");     z=m.count("Z"); 
+    print("A: ",a);     print("B: ",b);     print("C: ",c);     print("D: ",d);
+    print("E: ",e);     print("F: ",f);     print("G: ",g);     print("H: ",h);
+    print("I: ",i);     print("J: ",j);     print("K: ",k);     print("L: ",l);
+    print("M: ",M);     print("N: ",n);     print("O: ",o);     print("P: ",p);
+    print("Q: ",q);     print("R: ",r);     print("S: ",s);     print("T: ",t);
+    print("U: ",u);     print("V: ",v);     print("W: ",w);     print("X: ",x);
+    print("Y: ",y);     print("Z: ",z);
+n=str(input("Enter the string"))
+(most_frequent(n));
+      
+      
